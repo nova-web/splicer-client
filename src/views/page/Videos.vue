33ed6865002d5c448d81e081a9b02dd1e7adfb1e
qiangshushu <597515427@qq.com>
2018-12-11 16:41:55 +0800

    video

--- src/views/page/Videos.vue
+++ src/views/page/Videos.vue
@@ -1,31 +1,85 @@
 <template>
   <div>
+    <h1>RTMP:</h1>
+    <div class="row">
+      <div class="source">
+        <div id="rtmp1"></div>
+      </div>
+      <div class="source">
+        <div id="rtmp2"></div>
+      </div>
+    </div>
+    <h1>HLS:</h1>
+    <div class="row">
+      <div class="source">
+        <div id="hls1"></div>
+      </div>
+      <div class="source">
+        <div id="hls2"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import bus from '../common/bus'
+  import bus from '../common/bus';
+  // import videojs from 'video.js';
+
   export default {
-    name: 'webpack',
-    data() {
-      return {
-      }
+    mounted() {
+      this.playHLS('hls1', 'http://172.16.6.200/hls1/test.m3u8');
+      this.playHLS('hls2', 'http://172.16.6.200/hls2/test.m3u8');
+      this.playRTMP('rtmp1', 'rtmp://172.16.6.200/rtmp1/test');
+      this.playRTMP('rtmp2', 'rtmp://172.16.6.200/rtmp2/test');
     },
-    components: {
-    },
-    computed: {
-    },
-    created() {
-    },
-    // deactivated() {
-    //   window.removeEventListener('resize', this.renderChart)
-    //   bus.$off('collapse', this.handleBus)
-    // },
     methods: {
+      playHLS(id, src) {
+        cyberplayer(id).setup({
+          width: 512,
+          height: 384,
+          // width: 670,
+          // height: 480,
+          isLive: true,
+          file: src,
+          // file: 'http://cyberplayerplay.kaywang.cn/cyberplayer/demo201711-L1.m3u8',
+          autostart: true,
+          stretching: 'uniform',
+          repeat: false,
+          volume: 100,
+          controls: true,
+          hls: {
+            reconnecttime: 5 // hls直播的重连次数
+          },
+          ak: 'cc94de6803904f2ca7159eedeaced55d'
+        });
+      },
+      playRTMP(id, src) {
+        cyberplayer(id).setup({
+          width: 512,
+          height: 384,
+          file: src,
+          // file: 'rtmp://cyberplayerplay.kaywang.cn/cyberplayer/demo201711-L1',
+          autostart: true,
+          stretching: 'uniform',
+          repeat: false,
+          volume: 100,
+          controls: true,
+          rtmp: {
+            reconnecttime: 5, // rtmp直播的重连次数
+            bufferlength: 1 // 缓冲多少秒之后开始播放 默认1秒
+          },
+          ak: 'cc94de6803904f2ca7159eedeaced55d'
+        });
+      }
     }
   }
-
 </script>
 
-<style scoped>
+<style scoped lang="less">
+  .row {
+    display: flex;
+    .source {
+      margin-right: 10px;
+    }
+  }
 </style>
